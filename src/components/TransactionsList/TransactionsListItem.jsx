50a0ced88257bd7801860dcb29b7b1fc5ccb0989
II-777 <45958058+II-777@users.noreply.github.com>
2024-07-22 17:21:19 -0400

    update task 3

--- src/components/TransactionsList/TransactionsListItem.jsx
+++ src/components/TransactionsList/TransactionsListItem.jsx
@@ -1,9 +1,9 @@
+const capitalizeFirstLetter = string => string ? `${string[0].toUpperCase()}${string.slice(1)}` : '';
+
 export const TransactionsListItem = ({ type, amount, currency }) => (
-  <>
-    <tr>
-      <td>{type}</td>
-      <td>{amount}</td>
-      <td>{currency}</td>
-    </tr>
-  </>
+  <tr>
+    <td>{capitalizeFirstLetter(type)}</td>
+    <td>{amount}</td>
+    <td>{currency}</td>
+  </tr>
 );
